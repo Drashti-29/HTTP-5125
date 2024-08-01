7edd9292f244505ea9b773bfffc089940b9e4680
Drashti-29 <pokiyadrashti29@gmail.com>
2024-08-01 19:00:19 -0400

    Adding <form method=POST action=/Teacher/Create>

---
+++ HTTP-5125-assignment4-DrashtiPokiya/Controllers/TeacherController.cs
@@ -0,0 +1,115 @@
+﻿using Assignment3.Models;
+using Mysqlx.Datatypes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Assignment3.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        // This action method handles requests to the base URL for the Teacher controller.
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Teacher/List
+        // This action method handles requests to the /Teacher/List URL.
+        // It optionally accepts a SearchKey parameter to filter the list of teachers.
+        public ActionResult List(string SearchKey = null)
+        {
+            // Create an instance of the TeacherDataController to access its methods.
+            TeacherDataController controller = new TeacherDataController();
+
+            // Call the ListTeacher method of TeacherDataController to get the list of teachers.
+            // Pass the SearchKey to filter the results.
+            IEnumerable<Teacher> teacher = controller.ListTeacher(SearchKey);
+
+            // Return the View with the list of teachers.
+            return View(teacher);
+        }
+
+        // GET: /Teacher/Show
+        // This action method handles requests to the /Teacher/Show URL.
+        // It accepts an id parameter to retrieve details of a specific teacher.
+        public ActionResult Show(int id)
+        {
+            // Create an instance of the TeacherDataController to access its methods.
+            TeacherDataController controller = new TeacherDataController();
+
+            // Call the FindTeacher method of TeacherDataController to get details of the specified teacher.
+            Teacher NewTeacher = controller.FindTeacher(id);
+
+            // Return the View with the details of the teacher.
+            return View(NewTeacher);
+        }
+
+        //GET : /Teacher/DeleteConfirm/{id}
+        public ActionResult DeleteConfirm(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.FindTeacher(id);
+
+
+            return View(NewTeacher);
+        }
+
+        //POST : /Teacher/Delete/{id}
+        [System.Web.Http.HttpPost]
+        public ActionResult Delete(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            controller.DeleteTeacher(id);
+            return RedirectToAction("List");
+        }
+
+        //GET : /Teacher/New
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        //GET : /Teacher/Ajax_New
+        public ActionResult Ajax_New()
+        {
+            return View();
+
+        }
+
+        //POST : /Teacher/Create
+        [System.Web.Http.HttpPost]
+        public ActionResult Create(int teacherid, string TeacherFname, string TeacherLname, decimal salary, string employeeNumber, DateTime hireDate)
+        {     
+            //Identify that this method is running
+            //Identify the inputs provided from the form
+
+            Debug.WriteLine("I have accessed the Create Method!");
+            Debug.WriteLine(teacherid);
+            Debug.WriteLine(TeacherFname);
+            Debug.WriteLine(TeacherLname);
+            Debug.WriteLine(salary);
+            Debug.WriteLine(employeeNumber);
+            Debug.WriteLine(hireDate);
+
+            Teacher NewTeacher = new Teacher();
+            NewTeacher.teacherId = teacherid;
+            NewTeacher.teacherFname = TeacherFname;
+            NewTeacher.teacherLname = TeacherLname;
+            NewTeacher.Salary = salary;
+            NewTeacher.employeeNumber = employeeNumber;
+            NewTeacher.HireDate = hireDate;
+
+
+            TeacherDataController controller = new TeacherDataController();
+            controller.AddTeacher(NewTeacher);
+
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ HTTP-5125-assignment4-DrashtiPokiya/Controllers/TeacherDataController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Assignment3.Models;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace Assignment3.Controllers
+{
+    public class TeacherDataController : ApiController
+    {
+        // The database context class which allows us to access our MySQL Database.
+        private SchoolDbContext Blog = new SchoolDbContext();
+
+        [HttpGet]
+        [Route("api/TeacherData/ListTeacher/{SearchKey?}")]
+        public IEnumerable<Teacher> ListTeacher(string SearchKey = null)
+        {
+            //Create an instance of a connection
+            MySqlConnection Conn = Blog.AccessDatabase();
+
+            //Open the connection between the web server and database
+            Conn.Open();
+
+            //Establish a new command (query) for our database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            // SQL QUERY
+            cmd.CommandText = "Select * from teachers where lower(teacherfname) like lower(@key) or lower(teacherlname) like lower(@key) or lower(concat(teacherfname, ' ', teacherlname)) like lower(@key)";
+            cmd.Parameters.AddWithValue("@key", "%" + SearchKey + "%");
+            cmd.Prepare();
+
+            //Gather Result Set of Query into a variable
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+            //Create an empty list of Teacher
+            List<Teacher> teachers = new List<Teacher> { };
+            //Loop Through Each Row the Result Set
+            while (ResultSet.Read())
+            {
+                //Access Column information by the DB column name as an index
+                int teachersId = Convert.ToInt32(ResultSet["teacherid"]);
+                string teachersFname = ResultSet["teacherfname"].ToString();
+                string teachersLname = ResultSet["teacherlname"].ToString();
+                string employeeNumber = ResultSet["employeenumber"].ToString();
+                Decimal teachersSalary = Convert.ToDecimal(ResultSet["salary"]);
+                DateTime hireDate = Convert.ToDateTime(ResultSet["hiredate"]);
+
+                Teacher NewTeacher = new Teacher();
+                NewTeacher.teacherId = teachersId;
+                NewTeacher.teacherLname = teachersLname;
+                NewTeacher.teacherFname = teachersFname;
+                NewTeacher.employeeNumber = employeeNumber;
+                NewTeacher.Salary = teachersSalary;
+
+                //Add the teachers Name to the List
+                teachers.Add(NewTeacher);
+            }
+
+            //Close the connection between the MySQL Database and the WebServer
+            Conn.Close();
+
+            //Return the final list of author names
+            return teachers;
+        }
+        [HttpGet]
+        public Teacher FindTeacher(int id)
+        {
+            Teacher NewTeacher = new Teacher();
+
+            //Create an instance of a connection
+            MySqlConnection Conn = Blog.AccessDatabase();
+
+            //Open the connection between the web server and database
+            Conn.Open();
+
+            //Establish a new command (query) for our database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            //SQL QUERY
+            cmd.CommandText = "Select * from teachers where teacherid = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Prepare();
+
+            //Gather Result Set of Query into a variable
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            while (ResultSet.Read())
+            {
+                //Access Column information by the DB column name as an index
+                int teachersId = Convert.ToInt32(ResultSet["teacherid"]);
+                string teachersFname = ResultSet["teacherfname"].ToString();
+                string teachersLname = ResultSet["teacherlname"].ToString();
+                string employeeNumber = ResultSet["employeenumber"].ToString();
+                DateTime hireDate = Convert.ToDateTime(ResultSet["hiredate"]);
+                Decimal teachersSalary = Convert.ToDecimal(ResultSet["salary"]);
+
+                NewTeacher.teacherId = teachersId;
+                NewTeacher.teacherLname = teachersLname;
+                NewTeacher.teacherFname = teachersFname;
+                NewTeacher.Salary = teachersSalary;
+                NewTeacher.employeeNumber = employeeNumber;
+                NewTeacher.HireDate = hireDate;
+            }
+            return NewTeacher;
+        }
+
+        /// <summary>
+        /// Deletes an Teacher from the connected MySQL Database if the ID of that author exists. Does NOT maintain relational integrity.
+        /// </summary>
+        /// <param name="id">The ID of the Teacher.</param>
+        /// <example>POST /api/TeacherData/DeleteTeacher/3</example>
+        [HttpPost]
+        public void DeleteTeacher(int id)
+        {
+            //Create an instance of a connection
+            MySqlConnection Conn = Blog.AccessDatabase();
+
+            //Open the connection between the web server and database
+            Conn.Open();
+
+            //Establish a new command (query) for our database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            //SQL QUERY
+            cmd.CommandText = "Delete from teachers where teacherid=@id";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+
+            Conn.Close();
+        }
+
+        /// <summary>
+        /// Adds an Teacher to the MySQL Database.
+        /// </summary>
+        /// <param name="NewTeacher">An object with fields that map to the columns of the author's table.</param>
+        /// <example>
+        /// POST api/TeacherData/AddTeacher 
+        /// FORM DATA / POST DATA / REQUEST BODY 
+        /// {
+        ///	"AuthorFname":"Christine",
+        ///	"AuthorLname":"Bittle",
+        ///	"AuthorBio":"Likes Coding!",
+        ///	"AuthorEmail":"christine@test.ca"
+        /// }
+        /// </example>
+        [HttpPost]
+     //  [EnableCors(origins: "*", methods: "*", headers: "*")]
+        public void AddTeacher([FromBody] Teacher NewTeacher)
+        {
+            //Create an instance of a connection
+            MySqlConnection Conn = Blog.AccessDatabase();
+
+            Debug.WriteLine(NewTeacher.teacherFname);
+
+            //Open the connection between the web server and database
+            Conn.Open();
+
+            //Establish a new command (query) for our database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            //SQL QUERY
+            cmd.CommandText = "insert into teachers (teacherid, teacherfname, teacherlname, employeenumber, hiredate, salary) " +
+                "values (@teacherid,@teacherfname,@teacherlname, @employeenumber, @hiredate, @salary)";
+            cmd.Parameters.AddWithValue("@teacherid", NewTeacher.teacherId);
+            cmd.Parameters.AddWithValue("@teacherfname", NewTeacher.teacherFname);
+            cmd.Parameters.AddWithValue("@teacherlname", NewTeacher.teacherLname);
+            cmd.Parameters.AddWithValue("@employeenumber", NewTeacher.employeeNumber);
+            cmd.Parameters.AddWithValue("@hiredate", NewTeacher.HireDate);
+            cmd.Parameters.AddWithValue("@salary", NewTeacher.Salary);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+
+            Conn.Close();
+        }
+    }
+}
